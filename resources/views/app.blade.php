76feab22901cce0f47f4513ed4c4d44082899014
monl1ght <muhammadbahaudinnajib@gmail.com>
2026-05-16 10:28:28 +0700

    Mengganti favicon HMPS RPL

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,11 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    <title inertia>{{ config('app.name', 'HMPS RPL Polibang') }}</title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="/favicon.ico?v=2" type="image/x-icon">
+    <link rel="shortcut icon" href="/favicon.ico?v=2" type="image/x-icon">
 
     @vite(['resources/js/app.js'])
     @inertiaHead
